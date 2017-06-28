e9f206c120f71a6a3bb75dcf9a042ea0751fa273
Simone Vittori <hello@simonewebdesign.it>
2017-06-28 12:24:38 +0100

    Add more tests

--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -5,8 +5,20 @@ defmodule BowlingTest do
     assert Bowling.start_game([]) == 0
   end
 
+  test "one roll" do
+    assert Bowling.start_game([3]) == 3
+  end
+
   test "a simple game frame" do
     assert Bowling.start_game([7,1]) == 8
+  end
+
+  test "three rolls, or one frame and a half" do
+    assert Bowling.start_game([7,1,3]) == 11
+  end
+
+  test "two simple game frames" do
+    assert Bowling.start_game([7,1,3,2]) == 13
   end
 
   test "when a spare is made, the bonus is the next roll" do
@@ -14,10 +26,22 @@ defmodule BowlingTest do
   end
 
   test "when a strike is made, the bonus is the sum of the next two rolls" do
-    assert Bowling.start_game([10,5,4]) == 38
+    assert Bowling.start_game([10,5,4]) == 28
+  end
+
+  test "double strike!" do
+    assert Bowling.start_game([10,10]) == 30
+  end
+
+  test "one gutter" do
+    assert Bowling.start_game([0,1,2,3]) == 6
+  end
+
+  test "three gutters" do
+    assert Bowling.start_game([0,0,0]) == 0
   end
 
   test "a perfect game" do
-    assert Bowling.start_game([10,10,10,10,10,10,10,10,10,10,10,10] == 300)
+    assert Bowling.start_game([10,10,10,10,10,10,10,10,10,10,10,10]) == 300
   end
 end
