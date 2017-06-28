3ba742ca7acb16b65b54116f7be7667bfa87273a
Simone Vittori <hello@simonewebdesign.it>
2017-06-28 15:02:56 +0100

    wip

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -4,35 +4,64 @@ defmodule Bowling do
   def start_game(rolls) do
     initial_state = %{
       score: 0,        # total score
-      bonus: nil,      # either :strike, :spare, or nil
-      half_frame: nil  # meta score
+      bonus: nil,      # either :strike, :spare or nil
+      # frame: 0         # meta score
+      frame: {1, []}   # first is current frame number, second is a list of
+                       # frame values, usually 2 (10th frame has 3 values)
     }
     %{score: final_score} = Enum.reduce(rolls, initial_state, fn roll, state ->
+      new_state =
+        case state.bonus do
+          nil ->
+            state
+
+          :spare ->
+            %{state |
+              score: state.score + roll,
+              bonus: nil
+            }
+
+          :strike ->
+            case state.frame do
+              0 ->
+                %{state |
+                  score: state.score + roll
+                }
+              _ ->
+                %{state |
+                  score: state.score + state.frame,
+                  bonus: nil
+                }
+            end
+        end
+
       case hd(rolls) do
         10 -> # strike!
-          %{state |
-            score: state.score + 10,
+          %{new_state |
+            score: new_state.score + 10,
             bonus: :strike
           }
 
         _ ->
-          case state.half_frame do
-            nil ->
-              %{state |
-                half_frame: roll
+          case new_state.frame do
+            0 ->
+              %{new_state |
+                score: new_state.score + roll,
+                frame: roll
               }
 
             _ ->
-              frame_score = state.half_frame + roll
-              if frame_score == 10 do
+              if new_state.frame + roll == 10 do
                 # spare!
-                %{state |
-                  score: state.score + frame_score,
-                  bonus: :spare
+                %{new_state |
+                  score: new_state.score + roll,
+                  bonus: :spare,
+                  frame: 0
                 }
               else
-                %{state |
-                  score: state.score + frame_score
+                %{new_state |
+                  score: new_state.score + roll,
+                  frame: 0
                 }
               end
           end
